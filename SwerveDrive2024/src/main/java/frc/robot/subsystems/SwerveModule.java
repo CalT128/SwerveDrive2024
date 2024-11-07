da351f720de661a633e2f6c9d931e8a83248d93d
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-11-07 08:50:45 -0500

    Initial commit

---
+++ SwerveDrive2024/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class SwerveModule {
+    TalonFX driveMotor;
+    TalonFX turnMotor;
+    CANcoder axisSensor;
+    double rotationalValue;
+    public SwerveModule(TalonFX dMotor, TalonFX tMotor, CANcoder aSensor, double rotationalValue){
+        driveMotor = dMotor;
+        turnMotor = tMotor;
+        axisSensor = aSensor;
+        this.rotationalValue = rotationalValue;
+    }
+}
