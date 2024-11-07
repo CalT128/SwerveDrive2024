fe8b331c8d5b9080678978611d5f60eeed530d7e
AdrianK47 <92941727+CalT128@users.noreply.github.com>
2024-11-07 15:12:01 -0500

    11/7

--- SwerveDrive2024/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ SwerveDrive2024/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -4,12 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
+  //MOTOR DECLARATION
   //FRONT LEFT MOTORS
   TalonFX frontLeftDriveMotor;
   TalonFX frontLeftTurnMotor;
@@ -22,12 +24,34 @@ public class SwerveSubsystem extends SubsystemBase {
   //BACK RIGHT MOTORS
   TalonFX backRightDriveMotor;
   TalonFX backRightTurnMotor;
-  //SWERVE MODULES
+  //SWERVE MODULE ENCODERS DECLARATION
+  CANcoder frontLeftEncoder;
+  CANcoder frontRightEncoder;
+  CANcoder backLeftEncoder;
+  CANcoder backRightEncoder;
+  //SWERVE MODULES DECLARATION
   SwerveModule frontLeftSwerveModule;
   SwerveModule frontRightSwerveModule;
   SwerveModule backLeftSwerveModule;
   SwerveModule backRightSwerveModule;
-  public SwerveSubsystem() {}
+  public SwerveSubsystem() {
+    //SWERVE MOTORS INSTANTIATION
+    frontLeftDriveMotor = new TalonFX(1,"Drivetrain");
+    frontLeftTurnMotor = new TalonFX(2,"Drivetrain");
+    frontRightDriveMotor = new TalonFX(3,"Drivetrain");
+    frontRightTurnMotor = new TalonFX(4,"Drivetrain");
+    backLeftDriveMotor = new TalonFX(7,"Drivetrain");
+    backLeftTurnMotor = new TalonFX(8,"Drivetrain");
+    backRightDriveMotor = new TalonFX(5,"Drivetrain");
+    backRightTurnMotor = new TalonFX(6,"Drivetrain");
+    //ENCODER INSTANTIATION
+    frontLeftEncoder = new CANcoder(20,"Drivetrain");
+    frontRightEncoder = new CANcoder(21,"Drivetrain");
+    backLeftEncoder = new CANcoder(22,"Drivetrain");
+    backRightEncoder = new CANcoder(23,"Drivetrain");
+    frontLeftSwerveModule = new SwerveModule(frontLeftDriveMotor,frontLeftTurnMotor,frontLeftEncoder,45);
+
+  }
 
   @Override
   public void periodic() {
